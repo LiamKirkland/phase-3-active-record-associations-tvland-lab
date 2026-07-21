df709e5e32a003a1fd08c79ee986a33d757b10bd
LiamKirkland <liam.b.kirkland@gmail.com>
2026-07-21 16:19:20 -0400

    AR Tv-Land Lab

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,9 +31,22 @@ end
 
 def migrate!(direction, version)
   migrations_paths = ActiveRecord::Migrator.migrations_paths
-  migrations = ActiveRecord::MigrationContext.new(migrations_paths, ActiveRecord::SchemaMigration).migrations
+  context = ActiveRecord::MigrationContext.new(migrations_paths)
 
   ActiveRecord::Migration.suppress_messages do
-    ActiveRecord::Migrator.new(direction, migrations, ActiveRecord::SchemaMigration, version).migrate
+    if direction == :down
+      if version == 0
+        # Target 0 means roll back ALL migrations completely
+        context.migrate(0)
+      else
+        # Roll back to the specific version state
+        context.migrate(version)
+      end
+    elsif direction == :up
+      # migrate(version) runs all migrations up to and including this version number
+      context.migrate(version)
+    end
   end
 end
+
+
