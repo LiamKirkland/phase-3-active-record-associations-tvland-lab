4bbf70d5df0166c65cf9152563a738a6736c36b7
Liam Kirkland <32485432+LiamKirkland@users.noreply.github.com>
2026-07-22 09:54:30 -0400

    Update network spec to include new method.

--- spec/models/04_network_spec.rb
+++ spec/models/04_network_spec.rb
@@ -1,26 +1,35 @@
 describe Network do
-  let(:nbc) { Network.first }
+  let(:adult_swim) { Network.first }
 
   before do
-    nbc = Network.create(channel: 4, call_letters: "NBC")
-    Show.create(name: "Community", network_id: nbc.id)
+    network = Network.create(channel: 50, call_letters: "AS")
+    show = Show.create(name: "Aqua Teen Hunger Force", network: network)
+    
+    snyder = Actor.create(first_name: "Dana", last_name: "Snyder")
+    means  = Actor.create(first_name: "Carey", last_name: "Means")
+    willis = Actor.create(first_name: "Dave", last_name: "Willis")
+
+    Character.create(name: "Master Shake", show_id: show.id, actor_id: snyder.id)
+    Character.create(name: "Frylock", show_id: show.id, actor_id: means.id)
+    Character.create(name: "Meatwad", show_id: show.id, actor_id: willis.id)
   end
 
   it "has a channel and call_letters" do
-    expect(nbc).to have_attributes(channel: 4, call_letters: "NBC")
+    expect(adult_swim).to have_attributes(channel: 50, call_letters: "AS")
   end
 
   describe "#shows" do
     it "returns the shows associated with the network" do
-      community = Show.find_by(name: "Community")
-      expect(nbc.shows).to include(community)
+      athf = Show.find_by(name: "Aqua Teen Hunger Force")
+      expect(adult_swim.shows).to include(athf)
     end
   end
 
-  describe "#sorry" do
-    it "apologizes for passing on John Mulaney's Pilot" do
-      expect(nbc.sorry).to eq("We're sorry about passing on John Mulaney's pilot")
+  describe "#first_show_actors" do
+    it "returns a string containing the show name, actors (full names), and their characters" do
+      result = adult_swim.first_show_actors
+
+      expect(result).to include("Aqua Teen Hunger Force", "Dana Snyder", "Master Shake", "Carey Means", "Frylock", "Dave Willis", "Meatwad")
     end
   end
-  
 end
